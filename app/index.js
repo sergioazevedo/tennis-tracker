21fc410c1e8224fb1b332e65b7e0f7073373e345
Sergio Azevedo <sergior.jr@gmail.com>
2019-05-26 14:43:12 +0200

    adds main container

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+import document from "document";
+
+let container = document.getElementById("app-container");
+
+// Get the selected index
+let currentIndex = container.value;
+
+// Set the selected index
+container.value = 0; // jump to first slide
